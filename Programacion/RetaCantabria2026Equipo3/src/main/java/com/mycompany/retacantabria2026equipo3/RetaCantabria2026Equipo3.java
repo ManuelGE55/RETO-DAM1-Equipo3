fbd473ed606e1574295d1ced2b7a77a5f2c6c8da
Hugo <hfernandezc02@educantabria.es>
2026-05-05 17:44:35 +0200

    creacion app
    a

---
+++ Programacion/RetaCantabria2026Equipo3/src/main/java/com/mycompany/retacantabria2026equipo3/RetaCantabria2026Equipo3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.retacantabria2026equipo3;
+
+/**
+ *
+ * @author DAM212
+ */
+public class RetaCantabria2026Equipo3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
