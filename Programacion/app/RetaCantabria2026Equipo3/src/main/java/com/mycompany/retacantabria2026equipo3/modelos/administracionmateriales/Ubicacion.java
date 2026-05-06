a1c4bfb186acd940752dd3a4b14c2910594d1e54
Hugo <hfernandezc02@educantabria.es>
2026-05-06 17:23:38 +0200

    rellenar clase de ubicacion

--- Programacion/app/RetaCantabria2026Equipo3/src/main/java/com/mycompany/retacantabria2026equipo3/modelos/administracionmateriales/Ubicacion.java
+++ Programacion/app/RetaCantabria2026Equipo3/src/main/java/com/mycompany/retacantabria2026equipo3/modelos/administracionmateriales/Ubicacion.java
@@ -4,10 +4,25 @@
  */
 package com.mycompany.retacantabria2026equipo3.modelos.administracionmateriales;
 
+import java.awt.List;
+import java.util.ArrayList;
+
 /**
  *
  * @author DAM212
  */
-public class Ubicacion {
+public abstract class Ubicacion {
     
+    protected List<Material> materiales;
+    
+    public Ubicacion() {
+        this.materiales = new ArrayList<>();
+    }
+    
+    public boolean addMaterial(Material material) {
+        return materiales.add(material);
+    }
+    
+    @Override
+    public abstract String toString();
 }
