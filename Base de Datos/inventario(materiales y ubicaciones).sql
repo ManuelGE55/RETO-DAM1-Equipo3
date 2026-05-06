85609b9630121f596b340db92cbb1a0761b3c553
Manuel González <langostamanolo@gmail.com>
2026-05-06 16:22:07 +0200

    Base de datos - Materiales y Ubicaciones

---
+++ Base de Datos/inventario(materiales y ubicaciones).sql
@@ -0,0 +1,26 @@
+CREATE DATABASE IF NOT EXISTS inventario;
+USE inventario;
+
+DROP TABLE IF EXISTS material;
+CREATE TABLE material(
+	id_material INT AUTO_INCREMENT,
+    nombre VARCHAR(30),
+    descripcion VARCHAR(60),
+    cantidad INT,
+    stock_minimo INT,
+    categoria ENUM("Hardware","Herramienta","Fungible"),
+    estado ENUM("Nuevo","En buen estado","A reparar","Roto"),
+    id_ubicacion INT,
+    PRIMARY KEY(id_material),
+    KEY (id_ubicacion)
+); 
+
+DROP TABLE IF EXISTS ubicacion;
+CREATE TABLE ubicacion(
+	id_ubicacion INT AUTO_INCREMENT,
+    armario INT,
+    balda INT,
+    cajon INT,
+    descripcion VARCHAR(50),
+    PRIMARY KEY(id_ubicacion)
+);
