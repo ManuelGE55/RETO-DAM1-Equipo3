461aa8382908b048232cbf7da086dbe56e1658b7
CiroGalanVertiz <152623830+CiroGalanVertiz@users.noreply.github.com>
2026-05-07 17:37:54 +0200

    a

--- Programacion/app/RetaCantabria2026Equipo3/src/main/java/com/mycompany/retacantabria2026equipo3/modelos/administracionmateriales/Material.java
+++ Programacion/app/RetaCantabria2026Equipo3/src/main/java/com/mycompany/retacantabria2026equipo3/modelos/administracionmateriales/Material.java
@@ -12,16 +12,66 @@ import com.mycompany.retacantabria2026equipo3.enums.Estado;
  * @author DAM212
  */
 public class Material {
+    private int id;
     private String nombre;
     private String descripcion;
+    private int cantidad;
+    private int stockMinimo;
     private Categoria categoria;
     private Estado estado;
+    private String idUbicacion;
 
-    public Material(String nombre, String descripcion, Categoria categoria, Estado estado) {
+    public Material(String nombre, String descripcion, int cantidad, int stockMinimo, Categoria categoria, Estado estado, String idUbicacion) {
+        
         this.nombre = nombre;
         this.descripcion = descripcion;
+        this.cantidad = cantidad;
+        this.stockMinimo = stockMinimo;
         this.categoria = categoria;
         this.estado = estado;
+        this.idUbicacion = idUbicacion;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public int getStockMinimo() {
+        return stockMinimo;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public String getIdUbicacion() {
+        return idUbicacion;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+    
+    
     
 }
