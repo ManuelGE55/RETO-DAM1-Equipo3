eec2e6d9ab0d02ef8806f0a1e31a08a7879d43e6
Hugo <hfernandezc02@educantabria.es>
2026-05-07 15:45:43 +0200

    empezar ficheros gestortrafico

--- Programacion/app/RetaCantabria2026Equipo3/src/main/java/com/mycompany/retacantabria2026equipo3/gestores/GestorTrafico.java
+++ Programacion/app/RetaCantabria2026Equipo3/src/main/java/com/mycompany/retacantabria2026equipo3/gestores/GestorTrafico.java
@@ -4,10 +4,36 @@
  */
 package com.mycompany.retacantabria2026equipo3.gestores;
 
+import com.mycompany.retacantabria2026equipo3.modelos.administracionmateriales.Inventario;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author DAM212
  */
 public class GestorTrafico {
+    
+    private static final File inventarioCSV = new File("/src/main/CSVs/inventarioCSV.xlsx");
+    
+    public static String cargarInventario(Inventario inventario) {
+        String devolverDatos = "";
+        String csvSplitBy = ",";
+        try (BufferedReader br = new BufferedReader(new FileReader(inventarioCSV))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] datos = linea.split(csvSplitBy);
+                for (String dato: datos) {
+                    devolverDatos += dato + " ";
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return devolverDatos;
+    }
+    
     
 }
