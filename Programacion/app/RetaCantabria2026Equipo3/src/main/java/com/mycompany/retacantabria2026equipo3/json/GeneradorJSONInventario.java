8ae38e7287e592dda5fbd94a4fb7b4cedb874297
SaulValdunciel <svalduncielg01@educantabria.es>
2026-05-14 15:57:00 +0200

    cambios en la web y nueva clase en java

---
+++ Programacion/app/RetaCantabria2026Equipo3/src/main/java/com/mycompany/retacantabria2026equipo3/json/GeneradorJSONInventario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.retacantabria2026equipo3.json;
+
+/**
+ *
+ * @author DAM127
+ */
+public class GeneradorJSONInventario {
+
+    public void generarJSONInventario() {
+
+    }
+}
