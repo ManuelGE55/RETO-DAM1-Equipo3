69ce30143619510e5bfc403f6ce02ade5d6733cd
SaulValdunciel <svalduncielg01@educantabria.es>
2026-05-20 19:40:18 +0200

    Update DatosNoAsignadosException.java

--- Programacion/app/RetaCantabria2026Equipo3/src/main/java/com/mycompany/retacantabria2026equipo3/exceptions/DatosNoAsignadosException.java
+++ Programacion/app/RetaCantabria2026Equipo3/src/main/java/com/mycompany/retacantabria2026equipo3/exceptions/DatosNoAsignadosException.java
@@ -5,11 +5,20 @@
 package com.mycompany.retacantabria2026equipo3.exceptions;
 
 /**
- *
- * @author DAM212
+ *Excepción personalizada lanzada cuando existen
+ * datos obligatorios sin asignar dentro del sistema.
+ * 
+ * Se utiliza para controlar errores relacionados con
+ * información incompleta o no definida.
+ * 
+ * @author Hugo Fernández Calzado
  */
 public class DatosNoAsignadosException extends Exception {
-    
+    /**
+     * Constructor de la excepción.
+     * 
+     * @author Hugo Fernández Calzado
+     */
     public DatosNoAsignadosException(String message) {
         super(message);
     }
